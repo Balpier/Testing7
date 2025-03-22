f824a6eebcd4349a2f9f320cbaebfb950d4ce04d
Balpier <108562940+Balpier@users.noreply.github.com>
2025-03-22 12:37:32 -0500

    prueba de los test

---
+++ test.java
@@ -0,0 +1,15 @@
+public class test {
+    public static void main (String[] args){
+        ChocolateBags cb = new ChocolateBags();
+        int result = cb.calculate(1, 1, 5);
+        int result2 = cb.calculate(10, 2, 15);
+        int result3 = cb.calculate(9, 2, 19);
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+
+
+
+
+    }
+}
