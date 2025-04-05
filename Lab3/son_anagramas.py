2a90b819237042c930ab82619c439fc836960df0
Balpier <108562940+Balpier@users.noreply.github.com>
2025-04-04 19:34:42 -0500

    prueba con  condición minúscula

--- Lab3/son_anagramas.py
+++ Lab3/son_anagramas.py
@@ -7,6 +7,8 @@ def son_anagramas(text1: str, text2: str) -> bool:
     #comparar palabras ordenadas algfabeticamente
     return sorted(text1) == sorted(text2)
 
-print(son_anagramas("Roma", "Amor"))  
+print(son_anagramas("Roma", "Amor")) 
 print(son_anagramas("Sergio", "Riesgo"))  
-print(son_anagramas("Hola", "Mundo"))  
+print(son_anagramas("Hola", "Mundo"))  #distintas palabras
+print(son_anagramas("roma", "amor"))  #con minuscula 
+
