3fb493256a941022f2c5b778a7116cc7d262e138
Balpier <108562940+Balpier@users.noreply.github.com>
2025-04-04 19:52:51 -0500

    escenario de test con casilla vacía y una con texto

--- Lab3/son_anagramas.py
+++ Lab3/son_anagramas.py
@@ -17,3 +17,4 @@ print(son_anagramas("7654", "5746"))  # con numeros
 print(son_anagramas(".@!!?", "?.!@!")) # con simbolos o signos
 print(son_anagramas("papá", "apap"))  #con acento
 print(son_anagramas("", ""))  # cadenas vacias
+print(son_anagramas("", "hola"))  # con texto y vacia
